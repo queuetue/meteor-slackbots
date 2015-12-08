6650e26df29da84f16c0024a25f68496d30d91c5
Scott Russell <queuetue@gmail.com>
2015-12-08 16:50:03 -0500

    Limit to server

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'queuetue:slackbots',
   version: '0.1.1',
   // Brief, one-line summary of the package.
-  summary: 'Slackbots NPM smart package for Meteor`',
+  summary: 'Slackbots NPM smart package for Meteor',
   // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/queuetue/meteor-slackbots',
   // By default, Meteor will default to using README.md for documentation.
