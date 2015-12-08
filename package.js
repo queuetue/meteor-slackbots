7b04b827b9ce5290dd7cf0332f4038b9ec44220e
Scott Russell <queuetue@gmail.com>
2015-12-08 15:02:45 -0500

    rename

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'queuetue:slackbots',
   version: '0.1.0',
   // Brief, one-line summary of the package.
-  summary: 'Websockets NPM smart package for Meteor`',
+  summary: 'Slackbots NPM smart package for Meteor`',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/queuetue/meteor-slackbots',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -17,12 +17,12 @@ Npm.depends({
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use('ecmascript');
-  api.addFiles('websockets.js');
+  api.addFiles('slackbots.js');
 });
 
 Package.onTest(function(api) {
   api.use('ecmascript');
   api.use('tinytest');
-  api.use('cafex:websockets');
-  api.addFiles('websockets-tests.js');
+  api.use('queuetue:slackbots');
+  api.addFiles('slackbots-tests.js');
 });
